9a7c710c52a56a4120bcd7502fcc78a3b0f5605d
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-25 12:33:17 -0400

    Wrote CourseManager Class. Method addCourse adds a course to courses table iff course is not in table. Method clearCourses clears courses table.

--- src/main/java/edu/virginia/cs/hw7/coursereviews/CourseManager.java
+++ src/main/java/edu/virginia/cs/hw7/coursereviews/CourseManager.java
@@ -1,9 +1,6 @@
 package edu.virginia.cs.hw7.coursereviews;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class CourseManager {
     private final DatabaseManager db;
@@ -45,41 +42,6 @@ public class CourseManager {
             statement.executeUpdate("DELETE FROM courses");
             db.connection.commit();
             System.out.println("All courses have been deleted from the database.");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.disconnect();
-        }
-    }
-
-    public Course getCourse(String courseID) {
-        db.connect();
-        try {
-            PreparedStatement statement = db.connection.prepareStatement(
-                    "SELECT * FROM courses WHERE id = ?");
-            statement.setString(1, courseID);
-            ResultSet result = statement.executeQuery();
-            if (result.next()) {
-                return new Course(result.getString("department"), result.getInt("catalog_number"));
-            } else {
-                return null;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.disconnect();
-        }
-    }
-
-    public boolean checkCourse(Course course) {
-        db.connect();
-        try {
-            PreparedStatement statement = db.connection.prepareStatement(
-                    "SELECT * FROM courses WHERE department = ? AND catalog_number = ?");
-            statement.setString(1, course.getDepartment());
-            statement.setInt(2, course.getCatalogNumber());
-            ResultSet result = statement.executeQuery();
-            return result.next();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {

49d4c0df792195eec0697f2ba4c4914c327af396
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-25 13:34:25 -0400

    Attempted ReviewsManager. Testing is needed.

--- src/main/java/edu/virginia/cs/hw7/coursereviews/ReviewsManager.java
+++ src/main/java/edu/virginia/cs/hw7/coursereviews/ReviewsManager.java
@@ -1,11 +1,6 @@
 package edu.virginia.cs.hw7.coursereviews;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 
 public class ReviewsManager {
     private final DatabaseManager db;
@@ -15,39 +10,17 @@ public class ReviewsManager {
     }
 
     public void addReview(Review review) {
-
         db.connect();
         try {
             PreparedStatement statement = db.connection.prepareStatement(
                     "INSERT INTO reviews (student_id, course_id, rating, comment) VALUES (?, ?, ?, ?)");
-            int studentID = getStudentID(review.getStudent());
-            int courseID = getCourseID(review.getCourse());
-            statement.setInt(1, studentID);
-            statement.setInt(2, courseID);
+            statement.setInt(1, getStudentID(review.getStudent()));
+            statement.setInt(2, getCourseID(review.getCourse()));
             statement.setInt(3, review.getRating());
             statement.setString(4, review.getComment());
             statement.executeUpdate();
             db.connection.commit();
             System.out.println("Review added to the database.");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.disconnect();
-        }
-    }
-
-    public void removeReview(Review review) {
-        int studentID = getStudentID(review.getStudent());
-        int courseID = getCourseID(review.getCourse());
-        db.connect();
-        try {
-            PreparedStatement statement = db.connection.prepareStatement(
-                    "DELETE FROM reviews WHERE student_id = ? AND course_id = ?");
-            statement.setInt(1, studentID);
-            statement.setInt(2, courseID);
-            statement.executeUpdate();
-            db.connection.commit();
-            System.out.println("Review removed from the database.");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
@@ -70,6 +43,7 @@ public class ReviewsManager {
     }
 
     private int getStudentID(Student student) {
+        db.connect();
         try {
             PreparedStatement statement = db.connection.prepareStatement(
                     "SELECT ID FROM students WHERE NAME = ?");
@@ -82,10 +56,13 @@ public class ReviewsManager {
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            db.disconnect();
         }
     }
 
     private int getCourseID(Course course) {
+        db.connect();
         try {
             PreparedStatement statement = db.connection.prepareStatement(
                     "SELECT ID FROM courses WHERE DEPARTMENT = ? AND CATALOG_NUMBER = ?");
@@ -99,50 +76,9 @@ public class ReviewsManager {
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public List<Review> getReviews(Course course) {
-        db.connect();
-        List<Review> reviews = new ArrayList<>();
-        try {
-            PreparedStatement statement = db.connection.prepareStatement(
-                    "SELECT r.*, s.name as student_name, s.password as student_password " +
-                            "FROM reviews r " +
-                            "JOIN students s ON r.student_id = s.id " +
-                            "WHERE r.course_id = ?");
-            statement.setInt(1, getCourseID(course));
-            ResultSet result = statement.executeQuery();
-            while (result.next()) {
-                String studentName = result.getString("student_name");
-                String studentPassword = result.getString("student_password");
-                Student student = new Student(studentName, studentPassword);
-
-                int rating = result.getInt("rating");
-                String comment = result.getString("comment");
-                reviews.add(new Review(student, course, comment, rating));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.disconnect();
-        }
-        return reviews;
-    }
-
-    public boolean checkReview(Review review) {
-        db.connect();
-        try {
-            PreparedStatement statement = db.connection.prepareStatement(
-                    "SELECT * FROM reviews WHERE student_id = ? AND course_id = ?");
-            statement.setInt(1, getStudentID(review.getStudent()));
-            statement.setInt(2, getCourseID(review.getCourse()));
-            ResultSet result = statement.executeQuery();
-            return result.next();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         } finally {
             db.disconnect();
         }
     }
+
 }

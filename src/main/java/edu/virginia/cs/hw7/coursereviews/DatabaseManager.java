61d46f368e68c86302e19a2eadf7f925afaf4447
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-25 13:34:40 -0400

    Started Main function testing in DataBaseManager

--- src/main/java/edu/virginia/cs/hw7/coursereviews/DatabaseManager.java
+++ src/main/java/edu/virginia/cs/hw7/coursereviews/DatabaseManager.java
@@ -1,9 +1,6 @@
 package edu.virginia.cs.hw7.coursereviews;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseManager {
     public static final String DB_NAME = "Reviews.sqlite3";
@@ -79,8 +76,21 @@ public class DatabaseManager {
 
     public static void main(String[] args) {
         DatabaseManager db = new DatabaseManager();
-        db.connect();
-        db.createTables();
-        db.disconnect();
+        CourseManager courseManager = new CourseManager(db);
+        StudentManager studentManager = new StudentManager(db);
+        ReviewsManager reviewsManager = new ReviewsManager(db);
+
+        Course cs2100 = new Course("CS", 2100);
+        Course cs2120 = new Course("CS", 2120);
+        Course cs2130 = new Course("CS", 2130);
+        Student Jason = new Student("Jason", "Jason_He");
+        Student Jacob = new Student("Jacob", "Jacob_He");
+        Student Brandon = new Student("Brandon", "Brandon_He");
+        Review jasonReview_1 = new Review(Jason, cs2100, "This is a great course", 5);
+        Review jasonReview_2 = new Review(Jason, cs2120, "This is a meh course", 3);
+        Review jasonReview_3 = new Review(Jason, cs2130, "This is a bad course", 1);
+        Review jacobReview_1 = new Review(Jacob, cs2100, "This is a TRASH course", 1);
+        Review jacobReview_2 = new Review(Jacob, cs2120, "This is an OK course", 2);
+        Review jacobReview_3 = new Review(Jacob, cs2130, "This is a GOOD course", 4);
     }
 }

43f02d05d12b0ad8310981d9eae3f25daff6a074
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-27 01:02:15 -0400

    Deleted un-needed Main class in coursereviews.

--- src/main/java/edu/virginia/cs/hw7/coursereviews/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.virginia.cs.hw7.coursereviews;
-
-public class Main {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        ui.startApplication();
-    }
-}

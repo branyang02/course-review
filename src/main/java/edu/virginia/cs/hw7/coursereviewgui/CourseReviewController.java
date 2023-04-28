ac8a7e40dc100cf3f8fa8fefa9b2893505f0eb91
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-28 13:50:19 -0400

    Started login page. Added login button.

--- src/main/java/edu/virginia/cs/hw7/coursereviewgui/CourseReviewController.java
+++ src/main/java/edu/virginia/cs/hw7/coursereviewgui/CourseReviewController.java
@@ -1,14 +1,36 @@
 package edu.virginia.cs.hw7.coursereviewgui;
 
+import edu.virginia.cs.hw7.coursereviews.CourseReviewsService;
+import edu.virginia.cs.hw7.coursereviews.Student;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class CourseReviewController {
     @FXML
-    private Label welcomeText;
-
+    private TextField usernameField;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField passwordField;
+    @FXML
+    private Label errorMessage;
+    private CourseReviewsService courseReviewsService;
+
+    public CourseReviewController() {
+        courseReviewsService = new CourseReviewsService();
+    }
+
+    public void loginButtonClicked() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        try {
+            courseReviewsService.login(new Student(username, password));
+            System.out.println("Login successful!");
+            // TODO: go to main menu.
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            errorMessage.setText(e.getMessage());
+        }
     }
 }

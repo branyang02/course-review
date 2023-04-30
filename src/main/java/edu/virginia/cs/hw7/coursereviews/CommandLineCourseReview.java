d3c0188cc17d6443746028b76192770e7fb7b20b
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-30 15:22:51 -0400

    Changed main class to CommandLineCourseReview. Run GUI on default, run command line interface with -c or --command.

--- src/main/java/edu/virginia/cs/hw7/coursereviews/CommandLineCourseReview.java
+++ src/main/java/edu/virginia/cs/hw7/coursereviews/CommandLineCourseReview.java
@@ -1,8 +1,14 @@
 package edu.virginia.cs.hw7.coursereviews;
 
+import edu.virginia.cs.hw7.coursereviewgui.CourseReviewApplication;
+
 public class CommandLineCourseReview {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        ui.startApplication();
+        if (args.length > 0 && (args[0].equals("-c") || args[0].equals("--command"))) {
+            ui.startApplication();
+        } else {
+            CourseReviewApplication.main(args);
+        }
     }
 }

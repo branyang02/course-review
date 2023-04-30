aebc927dddd54ec8dacd16b2617186141327efea
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-04-30 15:27:26 -0400

    Added error message for illegal flag in CommandLineCourseReview.

--- src/main/java/edu/virginia/cs/hw7/coursereviews/CommandLineCourseReview.java
+++ src/main/java/edu/virginia/cs/hw7/coursereviews/CommandLineCourseReview.java
@@ -5,10 +5,16 @@ import edu.virginia.cs.hw7.coursereviewgui.CourseReviewApplication;
 public class CommandLineCourseReview {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        if (args.length > 0 && (args[0].equals("-c") || args[0].equals("--command"))) {
-            ui.startApplication();
-        } else {
-            CourseReviewApplication.main(args);
+        if (args.length > 0) {
+            String flag = args[0];
+            if (flag.equals("-g") || flag.equals("--gui")) {
+                CourseReviewApplication.main(args);
+                return;
+            } else if (flag.equals("-c") || flag.equals("--command")) {
+                ui.startApplication();
+                return;
+            }
         }
+        System.err.println("Invalid flag. Usage: java -jar CourseReview.jar [-g|--gui|-c|--command]");
     }
 }
